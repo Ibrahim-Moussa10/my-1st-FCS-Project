0dc5ab58788c0ed9b3c356c3d4358d5558e47b48
Ibrahim Moussa <i.moussa.15.im@gmail.com>
2024-10-28 11:48:39 +0200

    defining driver and cities classes

---
+++ my-1st-project-FCS.py
@@ -0,0 +1,9 @@
+class Driver:
+    def __init__(self,id,name,start_city):
+        self.id=id
+        self.name=name
+        self.start_city=start_city
+class Cities:
+    def __init__(self,city,path):
+       self.city=city
+       self.path=path
