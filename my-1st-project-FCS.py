7bb0cdad2d73ff872f1b86e7093ef6a1e000b547
Ibrahim Moussa <i.moussa.15.im@gmail.com>
2024-10-28 12:10:06 +0200

    defining info display

--- my-1st-project-FCS.py
+++ my-1st-project-FCS.py
@@ -3,7 +3,11 @@ class Driver:
         self.id=id
         self.name=name
         self.start_city=start_city
+    def driverInfo(self):
+        print(f'ID:{self.id}, Name: {self.name},Start City: {self.start_city}')
 class Cities:
     def __init__(self,city,path):
        self.city=city
        self.path=path
+    def deliverInfo(self):
+        print(f'Cities: {self.city} Path: {self.path}')
