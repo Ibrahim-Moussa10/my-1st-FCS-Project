41cba3c85da2fb00e2e6e4257181421c4c8eae21
Ibrahim Moussa <i.moussa.15.im@gmail.com>
2024-10-28 13:51:48 +0200

    finishing add driver and show drivers list

--- my-1st-project-FCS.py
+++ my-1st-project-FCS.py
@@ -1,19 +1,19 @@
 class Driver:
-    def __init__(self,id,name,start_city):
-        self.id=id
+    def __init__(self, id ,name,start_city):
+        self.id = id
         self.name=name
         self.start_city=start_city
-    def driverInfo(self):
-        print(f'ID:{self.id}, Name: {self.name},Start City: {self.start_city}')
 
+    def __str__(self):
+        return f'ID: {self.id}, Name: {self.name}, Start City: {self.start_city}'
 class Cities:
 
     def __init__(self,city,path):
        self.city=city
        self.path=path
 
-    def deliverInfo(self):
-        print(f'Cities: {self.city} Path: {self.path}')
+    def __str__(self) -> str:
+        pass
 
 class DeliverySystem:
 
@@ -21,8 +21,16 @@ class DeliverySystem:
         self.drivers=[]
 
     def add_driver(self, id, name,start_city):
-        driver = Driver(id,name, start_city)
+        driver = Driver(id ,name, start_city)
         self.drivers.append(driver)
+        
+    def show_drivers(self):
+        if not self.drivers:
+            print("No Drivers available")
+        else:
+            for driver in self.drivers:
+                print(driver)
+
     def drivers_menu(self):
         while True:
             print("1. To view all the drivers")
@@ -31,11 +39,13 @@ class DeliverySystem:
             print("4. To go back to the main menu")
             choice = input('Enter your choice: ')
             if choice =='1':
-                pass
+                self.show_drivers()
             elif choice == '2':
                 id = input('Enter driver ID: ')
                 name= input('Enter new driver name: ')
                 start_city=input("Enter driver start city: ")
+                self.add_driver(id, name, start_city)
+                print("Driver was added")
             elif choice =='3':
                 pass
             elif choice =='4':
